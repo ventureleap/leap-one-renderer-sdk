550a6a4b888f5ced4834e1882b953e4104798d1d
GitHub Action <action@github.com>
2021-02-19 11:34:19 +0000

    Renderer Service commit hash: bdd2af7

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Renderer Service commit hash: 2bf48bc"
+    release_note="Renderer Service commit hash: bdd2af7"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
